eea673dafb4468fc8de3f68e4ff5ff37fac3bb43
Fereus <ivan439@yandex.ru>
2018-08-22 21:50:56 +0400

    Added comments

--- Data processing.py
+++ Data processing.py
@@ -1,16 +1,31 @@
+# -*- coding: utf-8 -*-
+
 import xlrd, xlwt
 
 def opening_and_reading_file(name):
+    """ Открывает файл и получает данные из .xls и записывает их в
+    переменную data.
+
+        data = [[str, ..., str], ..., [str, ..., str]]
+            data[0] - названия столбцов(номера вопросов)
+            data[i] - i-ая строка файла, содержащая ответы на вопросы
+    """
+
     rb = xlrd.open_workbook('data/{}.xls'.format(name), formatting_info = True)
     sheet = rb.sheet_by_index(0)
     data = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+
     return data
 
 def data_processing(data):
+    """ Получает из data вариантры ответов и сами ответы на вопрос"""
+    
     variants_answers = [[] for i in range(len(data[0]))]
     answers = [[] for i in range(len(data[0]))]
-    for i in range(len(data[0])): #Идём по столбцам 
-        for j in range(1, len(data)): #Идём по строкам
+
+    # Получаем ответы на вопрос
+    for i in range(len(data[0])):
+        for j in range(1, len(data)): 
             try:
                 answers[i].append(int(data[j][i]))
             except ValueError:
@@ -24,22 +39,41 @@ def data_processing(data):
                         except ValueError:
                             answers[i].append(k)
 
+    # Получаем возможные (уникальные) варианты ответов на вопрос
     for i in range(len(answers)):
         for j in range(len(answers[i])):
             if answers[i][j] not in variants_answers[i]:
                 variants_answers[i].append(answers[i][j])
+
     return answers, variants_answers
 
 def counter(answers, variants_answers):
+    """ Суммирует ответы на вопрос и записывает их в переменную additional_data,
+    где идет чередование столбцов с вариантами ответов и количеством этих
+    ответов.
+
+        additional_data = [[str, ..., str], ..., [str, ..., str]]
+            additional_data[i] - варианты ответов
+                i - even numbers
+            additional_data[j] - количество этих ответов
+                j - odd numbers
+    """
+
     additional_data = [[] for i in range(2 * len(data[0]))]
+
     for i in range(len(answers)):
         for j in variants_answers[i]:
             a = answers[i].count(j)
             additional_data[i * 2].append(j)
             additional_data[i * 2 + 1].append(a)
+
     return additional_data
 
 def recording_file(name, data, additional_data):
+    """ Записывает данные из переменной data в новый файл и добавляет в конце
+    данные из переменной additional_data.
+    """
+    
     sheet = xlwt.Workbook()
     ws = sheet.add_sheet('Лист1')
 
@@ -60,6 +94,7 @@ def recording_file(name, data, additional_data):
             for j in range(len(additional_data[i])):
                 ws.write(j + len(data) + 4, i, additional_data[i][j])
             k = True
+
     sheet.save(name + '_Перезаписанный' + '.xls')
 
 
